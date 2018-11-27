e15c5f1886df38edd36b0809bd6c0d1378e4eb98
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-27 19:09:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ function lowerCaseDrivers(drivers) {
 	return drivers.map(function(name) {return name.toLowerCase()});
 }
 
- function attributesToPhrase(drivers) {
+ function nameToAttributes(drivers) {
 	  return drivers.map(function(name) {
 		let driverName = name.split(" ");
 		return {
 			firstName: driverName[0], 
 			lastName: driverName[1]
 		} 
- 	})
+ 	} )
 }
  function attributesToPhrase(drivers) {
-	return drivers.map(function(driver) {
+  	return drivers.map(function(driver) {
 		return `${driver.name} is from ${driver.hometown}`;
 	})
+ }
